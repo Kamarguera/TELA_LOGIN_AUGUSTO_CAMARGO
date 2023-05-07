159cc2351bc2e6a33c033efe156081f04b8404ab
Augusto Cesar Camargo <gutto88@gmail.com>
2023-05-07 05:28:21 -0300

    Recover name and email from firebase

--- app/src/main/java/com/example/tela_login_augusto_camargo/TelaPrincipal.java
+++ app/src/main/java/com/example/tela_login_augusto_camargo/TelaPrincipal.java
@@ -1,5 +1,6 @@
 package com.example.tela_login_augusto_camargo;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,14 +10,19 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import java.util.Objects;
 
 public class TelaPrincipal extends AppCompatActivity {
     private Button btn_deslogar;
-
     private TextView nomeUsuario, emailUsuario;
-
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String usuarioID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,12 +39,35 @@ public class TelaPrincipal extends AppCompatActivity {
         });
     }
 
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+        String email = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getEmail();
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DocumentReference documentReference = db.collection("Usuarios").document(usuarioID);
+        documentReference.addSnapshotListener((documentSnapshot, error) -> {
+
+            if (documentSnapshot != null){
+                nomeUsuario.setText(documentSnapshot.getString("nome"));
+                emailUsuario.setText(email);
+            }
+
+
+
+        });
+
+
+    }
+
     private void TelaLogin() {
         Intent intent = new Intent(TelaPrincipal.this, FormLogin.class);
         startActivity(intent);
         finish();
     }
-
     private void IniciarComponentes() {
         nomeUsuario = findViewById(R.id.textNomeUsuario);
         emailUsuario = findViewById(R.id.textEmail);
